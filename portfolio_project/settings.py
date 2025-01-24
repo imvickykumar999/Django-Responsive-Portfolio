72c42850335d48e7938602694b7d12db27f67d09
Vicky Kumar <imvickykumar999@gmail.com>
2025-01-24 22:47:04 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k=9pn0ry4+y(d*j(!2ye7j!keup2f$$76b_4b*x2k+uk!erpmq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
