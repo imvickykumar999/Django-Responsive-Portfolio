93f7eda478bf08d88447e3d2eef7571a4b098a03
Vicky Kumar <imvickykumar999@gmail.com>
2025-01-24 23:04:25 +0530

    static folder

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', include('portfolio.urls')),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
